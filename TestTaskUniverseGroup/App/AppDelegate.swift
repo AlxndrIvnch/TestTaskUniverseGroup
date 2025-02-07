c27a3db9ca332e891eada14d7aa786ed3580db6f
Oleksandr Ivanchenko <alexandr.vanch@gmail.com>
2025-02-07 16:30:15 +0200

    project setup

---
+++ TestTaskUniverseGroup/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  TestTaskUniverseGroup
+//
+//  Created by Oleksandr Ivanchenko on 2/7/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
