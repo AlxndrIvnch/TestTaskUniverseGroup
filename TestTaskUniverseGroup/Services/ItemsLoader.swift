b4ddbed9a7f37aaeb6ae730350f1e7fddc9457b4
Oleksandr Ivanchenko <alexandr.vanch@gmail.com>
2025-03-19 10:53:13 +0200

    Refactored ItemsLoader to Rx

--- TestTaskUniverseGroup/Services/ItemsLoader.swift
+++ TestTaskUniverseGroup/Services/ItemsLoader.swift
@@ -6,28 +6,38 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-protocol ItemsLoaderProtocol: Actor {
-    func loadItems(progressHandler: (@Sendable (Double) -> Void)?) async throws -> [Item]
+protocol ItemsLoaderProtocol {
+    func loadItems(progress: PublishRelay<Float>?) -> Single<[Item]>
 }
 
 extension ItemsLoaderProtocol {
-    func loadItems() async throws -> [Item] {
-        return try await loadItems(progressHandler: nil)
+    func loadItems() -> Single<[Item]> {
+        return loadItems(progress: nil)
     }
 }
 
-actor ItemsLoader: ItemsLoaderProtocol {
-    
-    func loadItems(progressHandler: (@Sendable (Double) -> Void)? = nil) async throws -> [Item] {
-        let duration = 3.0
-        let tolerance = 0.1
-        let steps = 50
-        let sleepInterval = (duration - tolerance / 2 * Double(steps)) / Double(steps)
-        for step in 1...steps {
-            try await Task.sleep(for: .seconds(sleepInterval), tolerance: .seconds(tolerance))
-            progressHandler?(Double(step) / Double(steps))
+final class ItemsLoader: ItemsLoaderProtocol {
+    func loadItems(progress: PublishRelay<Float>? = nil) -> Single<[Item]> {
+        let backgroundScheduler = ConcurrentDispatchQueueScheduler(qos: .background)
+        return Single.create { single in
+            let steps = 15
+            let items = (1...100).map { Item(id: $0, title: "Item \($0)", isFavorite: false) }
+            return Observable.range(start: 1, count: steps)
+                .concatMap { step in
+                    let delay = Int.random(in: 1...500)
+                    let progress = Float(Double(step) / Double(steps))
+                    return Observable.just(progress)
+                        .delay(.milliseconds(delay), scheduler: backgroundScheduler)
+                }
+                .subscribe(
+                    onNext: progress?.accept,
+                    onError: { single(.failure($0)) },
+                    onCompleted: { single(.success(items)) }
+                )
         }
-        return (1...100).map { Item(id: $0, title: "Item \($0)", isFavorite: false) }
+        .subscribe(on: backgroundScheduler)
     }
 }
