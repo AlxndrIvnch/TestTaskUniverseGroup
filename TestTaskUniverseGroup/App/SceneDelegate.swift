313401104de0e3513bf6fea7a91d002dba93d749
Oleksandr Ivanchenko <alexandr.vanch@gmail.com>
2025-03-26 23:50:59 +0200

    Change in 1

--- TestTaskUniverseGroup/App/SceneDelegate.swift
+++ TestTaskUniverseGroup/App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
+        print(1)
         guard let windowScene = scene as? UIWindowScene else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
